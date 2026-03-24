175798a1c1fba2a3b6ce19ea2b6dae012215fd40
Harshad-027 <harshadnikma@gmail.com>
2026-03-24 14:55:18 +0530

    customized browser icon

---
+++ src/app/icon.tsx
@@ -0,0 +1,38 @@
+import { ImageResponse } from 'next/og';
+
+// Route segment config
+export const runtime = 'edge';
+
+// Image metadata
+export const size = {
+  width: 32,
+  height: 32,
+};
+export const contentType = 'image/png';
+
+// Image generation
+export default function Icon() {
+  return new ImageResponse(
+    (
+      <div
+        style={{
+          fontSize: 18,
+          background: '#7c3aed', // primary violet color
+          width: '100%',
+          height: '100%',
+          display: 'flex',
+          alignItems: 'center',
+          justifyContent: 'center',
+          color: 'white',
+          fontWeight: 800,
+          borderRadius: '6px',
+        }}
+      >
+        HN
+      </div>
+    ),
+    {
+      ...size,
+    }
+  );
+}
